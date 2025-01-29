c615eefd8bc5395227f36f0d607d2f103a2fc1b5
Saad-hassan78 <saadhassan9577@gmail.com>
2025-01-29 22:36:16 +0500

    Update first_file.cs

--- first_file.cs
+++ first_file.cs
@@ -6,7 +6,8 @@ class Program
     {
         Console.WriteLine("Your Name");
         Console.WriteLine("Your Roll Number");
-        Console.WriteLine("FSc Subjects: Physics, Computer, Math");
+        Console.WriteLine("FSc Subjects: Physics, Chemistry, Math");
         Console.WriteLine("1st Semester Subjects: Programming, Discrete Mathematics");
+        Console.WriteLine("2nd Semester Subjects: Algorithms, Data Structures, Database Systems");
     }
 }
