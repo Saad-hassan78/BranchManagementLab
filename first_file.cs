08e5d46bf8aa138b6d0cfd66393442c3d6bca148
Saad-hassan78 <saadhassan9577@gmail.com>
2025-01-29 22:28:22 +0500

    Update first_file.cs

--- first_file.cs
+++ first_file.cs
@@ -4,7 +4,8 @@ class Program
 {
     static void Main()
     {
-        Console.WriteLine("Muhammad  Saad Hassan");
+        Console.WriteLine("Muhammad Saad Hassan");
         Console.WriteLine("24-ST-058");
+        Console.WriteLine("FSc Subjects: Physics, Computer, Math");
     }
 }
