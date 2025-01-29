e79c7cf70df86b70eb2a23ce016ab6977a0d9da1
Saad-hassan78 <saadhassan9577@gmail.com>
2025-01-29 22:48:59 +0500

    Update first_file.cs

--- first_file.cs
+++ first_file.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main()
     {
+        Console.WriteLine("Summary: This file contains my name, roll number, FSc subjects, 1st semester subjects, and 2nd semester subjects.");
         Console.WriteLine("Your Name");
         Console.WriteLine("Your Roll Number");
         Console.WriteLine("FSc Subjects: Physics, Chemistry, Math");
